307c4cfe82bcdf94d6b0f614581231581bf72c82
Phoebe <minhphuong227@gmail.com>
2018-07-04 09:52:54 -0400

    NYT API

--- week4/API.py
+++ week4/API.py
@@ -1,31 +1,58 @@
 import requests
 import json
 import sys
+import time
+import os
 
 ARTICLE_SEARCH_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
 
+#Your code should take an API key, section name, and number of articles as command line arguments
+#write out a tab-delimited file where each article is in a separate row, with section_name, web_url, pub_date, and snippet as columns
 if __name__=='__main__':
-   if len(sys.argv) != 4:
-      sys.stderr.write('usage: %s <api_key><section_name><num_article>\n' % sys.argv[0])
-      sys.exit(1)
+    if len(sys.argv) != 3:
+        sys.stderr.write('usage: %s <section> <number of article>\n' % sys.argv[0])
+        sys.exit(1)
+    
+    #THis is to replace command line
+    api_key = '48963ca014a045bfb79f57923cfd767d'
+    #section_name = 'world'
+    #num_article = '1000'
 
-   api_key = sys.argv[1]
-   section_name = sys.argv[2]
-   num_artical = sys.argv[3]
-   
-   heading = ['section_name', 'web_url', 'pub_date', 'snippet']
+    section_name = sys.argv[1]    
+    num_article = sys.argv[2]
+    total_page = 0
+    if int(num_article) >=10:
+        if int(num_article) % 10 == 0:
+            total_page = int(int(num_article)/10)
+        else:
+            total_page = int(int(num_article)/10) + 1
 
-   f = open('nyt_article.tsv', 'w')
-   f.write('\t', join(heading))
+    #header ofthe file
+    heading = ['section_name', 'web_url', 'pub_date', 'snippet']
+    filename = 'nyt_article_' + section_name + '.tsv'
+    fileDir = r'C:\Users\Phuong Nguyen\Documents\ds3\coursework\week4'
+    fullfilename = os.path.join(fileDir, filename)
+    
+    f = open(fullfilename, 'w')
+    f.write('\t'.join(heading) + '\n')
 
-   for i in range(int(num_article)/10+1):
-       params = {'api-key': api_key,
-       'fq': 'section_name:' + section_name,
-       'page': i,
-       'sort':'newest'}
-       r = requests.get(ARTICLE_SEARCH_URL, params)
-       data = json.loads(r.content)
-       
-       for doc in data['response']['docs']:
-           f.write('\t'.join(section_name, doc['web_url'], doc['pub_date'], doc['snippet'].strip('\n')))
-           print(doc['web_url'])
+    for i in range(0,total_page):
+        params = {'api-key': api_key,
+        'fq': 'section_name:' + section_name,
+        'page': i,
+        'sort':'newest'}
+        r = requests.get(ARTICLE_SEARCH_URL, params)
+        data = json.loads(r.content)
+        
+        for doc in data['response']['docs']:
+            dataList = [section_name, doc['web_url'], doc['pub_date'], doc['snippet'].strip('\n')]
+            f.write('\t'.join(dataList)+'\n') 
+        time.sleep(3)
+        print('page' + str(i))
+        
+    f.close()
+
+#You'll have to loop over pages of API results until you have enough articles
+#You'll want to remove any newlines from article snippets to keep each article on one line          
+        
+
